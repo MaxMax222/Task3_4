a526f93a43aa35cd0537dccf8e94fb2e4066e2e6
MaxMax222 <126956833+MaxMax222@users.noreply.github.com>
2023-03-04 17:16:02 +0200

    Added Function That Findes the length of the longest streak

--- Q28/Program.cs
+++ Q28/Program.cs
@@ -8,6 +8,27 @@ namespace Q28
 {
     class Program
     {
+        static int LongestStreak(int[] arr)
+        {
+            int streak = 1;
+            int longestStreak = streak;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i-1] == arr[i])
+                {
+                    streak++;
+                }
+                else
+                {
+                    if (streak > longestStreak)
+                    {
+                        longestStreak = streak;
+                    }
+                    streak = 1;
+                }
+            }
+            return longestStreak;
+        }
         static void Main(string[] args)
         {
 
